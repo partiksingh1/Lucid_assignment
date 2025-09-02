c909e2eae4d6ee5c1bbdbe64d4a69439426c2f97
partiksingh1 <partiksingh28@gmail.com>
2025-09-03 00:05:28 +0200

    server : cors fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Enable CORS for frontend communication
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'], // React dev server ports
+    origin: ['http://localhost:3000', 'http://localhost:5173'], // React dev server ports
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
